4e1b6bfced503a14855daf7265e3e4282673f9b9
kmebin <blowhuiee@gmail.com>
2021-05-05 11:57:40 +0900

    [add] 2116

--- BOJ/main.py
+++ BOJ/main.py
@@ -1,14 +1,34 @@
 import sys
-import itertools
 
-n, s = map(int, sys.stdin.readline().split())
-data = list(map(int, sys.stdin.readline().split()))
-cnt = 0
+n = int(sys.stdin.readline())
+dice = []
+for _ in range(n):
+    dice.append(list(map(int, sys.stdin.readline().split())))
+pair = [5, 3, 4, 1, 2, 0]  # 마주보는 쌍
+maxOfSum = 0
 
-for r in range(1, n + 1):
-    nCr = list(itertools.combinations(data, r))
-    for item in nCr:
-        if s == sum(item):
-            cnt += 1
 
-print(cnt)
+def getMaxSide(dice, bottom, top):
+    maxOfSide = 0
+    for i in range(6):
+        if dice[i] == bottom or dice[i] == top:
+            continue
+        maxOfSide = max(maxOfSide, dice[i])
+    return maxOfSide
+
+
+for i in range(6):
+    bottom = dice[0][i]
+    top = dice[0][pair[i]]
+    sumOfSide = getMaxSide(dice[0], bottom, top)
+
+    for j in range(1, n):
+        for k in range(6):
+            if top == dice[j][k]:
+                bottom = dice[j][k]
+                top = dice[j][pair[k]]
+                break
+        sumOfSide += getMaxSide(dice[j], bottom, top)
+    maxOfSum = max(maxOfSum, sumOfSide)
+
+print(maxOfSum)
