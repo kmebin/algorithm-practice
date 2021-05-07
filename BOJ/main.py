ea76f9b468d54100ac72bb30b7ee02e225065da9
kmebin <blowhuiee@gmail.com>
2021-05-07 10:13:52 +0900

    [add] 2878

--- BOJ/main.py
+++ BOJ/main.py
@@ -1,21 +1,35 @@
 import sys
 
-case = int(sys.stdin.readline())
+candy, n = map(int, sys.stdin.readline().split())
+target = [int(sys.stdin.readline()) for _ in range(n)]
+result = [0] * n
 
-for _ in range(case):
-    n = int(sys.stdin.readline())
-    applicant = []
-    cnt = 1
+target.sort(reverse=True)
+lack = sum(target) - candy
 
-    for _ in range(n):
-        applicant.append(list(map(int, sys.stdin.readline().split())))
+while lack > 0:
+    lack_cnt, n_cnt = 0, 0
 
-    applicant.sort()
-    target = applicant[0][1]
+    if lack >= n:
+        for i in range(n):
+            tmp = target[i] - result[i]
 
-    for i in range(1, n):
-        if target > applicant[i][1]:
-            cnt += 1
-            target = applicant[i][1]
+            if tmp >= lack // n:
+                result[i] += lack // n
+                lack_cnt += lack // n
+            else:
+                result[i] += tmp
+                lack_cnt += tmp
+                n_cnt += 1
 
-    print(cnt)
+        lack -= lack_cnt
+        n -= n_cnt
+    else:
+        for i in range(lack):
+            result[i] += 1
+            lack_cnt += 1
+
+        lack -= lack_cnt
+
+result = list(map(lambda x: x ** 2, result))
+print(sum(result) % 2 ** 64)
