2fb282b7a95d760ed5677c73293335942646b633
kmebin <blowhuiee@gmail.com>
2021-05-10 19:25:30 +0900

    [add] 1000
    A+B

--- BOJ/main.py
+++ BOJ/main.py
@@ -1,15 +1,3 @@
 import sys
 
-candy, n = map(int, sys.stdin.readline().split())
-target = [int(sys.stdin.readline()) for _ in range(n)]
-result = 0
-
-target.sort()
-lack = sum(target) - candy
-
-for i in range(n):
-    cnt = min(target[i], lack // (n - i))
-    lack -= cnt
-    result += cnt ** 2
-
-print(result % 2 ** 64)
+print(sum(map(int, sys.stdin.readline().split())))
