c70b6a8d7c64ff17946bfcf0b2532e1bd560044c
kmebin <blowhuiee@gmail.com>
2021-05-10 20:05:05 +0900

    [add] 2748
    피보나치 수 2

--- BOJ/main.py
+++ BOJ/main.py
@@ -1,4 +1,12 @@
 import sys
 
-A, B = map(int, sys.stdin.readline().split())
-print(A - B)
+n = int(sys.stdin.readline())
+fibo = [0] * (n + 1)
+
+for x in range(n + 1):
+    if x == 0 or x == 1:
+        fibo[x] = x
+    else:
+        fibo[x] = fibo[x - 1] + fibo[x - 2]
+
+print(fibo[n])
