a52c73a87b3263fd952385dc442d44f6ebadcb65
kmebin <blowhuiee@gmail.com>
2021-07-22 20:08:18 +0900

    [add] 개미 전사
    이코테 2021

--- BOJ/main.py
+++ BOJ/main.py
@@ -1,14 +1,15 @@
 import sys
 
+# 식량창고 개수
 n = int(sys.stdin.readline())
+# 각 식량창고에 저장된 식량의 개수
+k = list(map(int, sys.stdin.readline().split()))
+k.insert(0, 0)
 
+d = [0] * (n + 1)
+d[1] = k[1]
 
-def fibo(x):
-    if x == 0:
-        return 0
-    if x == 1:
-        return 1
-    return fibo(x - 1) + fibo(x - 2)
+for i in range(2, n + 1):
+    d[i] = max(d[i - 2] + k[i], d[i - 1])
 
-
-print(fibo(n))
+print(d[n])
